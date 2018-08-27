0de202043f6d15a144be81c4489a57b897b3a441
gwendal <skeudwen@gmail.com>
2018-08-27 16:42:58 +0200

    [WIP] Added search language in preferences

--- wiktionary/KeywordQueryEventListener.py
+++ wiktionary/KeywordQueryEventListener.py
@@ -3,6 +3,10 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class KeywordQueryEventListener(EventListener):
 
@@ -16,12 +20,14 @@ class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         data = event.get_argument()
+        language_pref = extension.preferences['language']
+
+        logger.debug(language_pref)
 
         if data:
             return self.get_action_to_render(name="Search for",
                     description=data,
-                    on_enter=OpenUrlAction("https://fr.wiktionary.org/wiki/%s" % data))
-
+                    on_enter=OpenUrlAction("https://%s.wiktionary.org/wiki/%s" % (language_pref, data)))
         else:
             return self.get_action_to_render(name="Type in your query",
                                              description="Example: def hurluberlu")
